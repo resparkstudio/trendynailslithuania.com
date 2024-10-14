0895df807328e737d19f2abffd0745794e1b1bd4
Rokas Reizgys <rokas.student@gmail.com>
2024-10-14 14:56:42 +0300

    add header menu registration

--- theme/functions.php
+++ theme/functions.php
@@ -7,7 +7,7 @@
  * @package _tw
  */
 
-if ( ! defined( '_TW_VERSION' ) ) {
+if (!defined('_TW_VERSION')) {
 	/*
 	 * Set the theme’s version number.
 	 *
@@ -15,10 +15,10 @@ if ( ! defined( '_TW_VERSION' ) ) {
 	 * to create your production build, the value below will be replaced in the
 	 * generated zip file with a timestamp, converted to base 36.
 	 */
-	define( '_TW_VERSION', '0.1.0' );
+	define('_TW_VERSION', '0.1.0');
 }
 
-if ( ! defined( '_TW_TYPOGRAPHY_CLASSES' ) ) {
+if (!defined('_TW_TYPOGRAPHY_CLASSES')) {
 	/*
 	 * Set Tailwind Typography classes for the front end, block editor and
 	 * classic editor using the constant below.
@@ -42,7 +42,7 @@ if ( ! defined( '_TW_TYPOGRAPHY_CLASSES' ) ) {
 	);
 }
 
-if ( ! function_exists( '_tw_setup' ) ) :
+if (!function_exists('_tw_setup')):
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -50,17 +50,18 @@ if ( ! function_exists( '_tw_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function _tw_setup() {
+	function _tw_setup()
+	{
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on _tw, use a find and replace
 		 * to change '_tw' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( '_tw', get_template_directory() . '/languages' );
+		load_theme_textdomain('_tw', get_template_directory() . '/languages');
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		/*
 		 * Let WordPress manage the document title.
@@ -68,20 +69,20 @@ if ( ! function_exists( '_tw_setup' ) ) :
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('post-thumbnails');
 
 		// This theme uses wp_nav_menu() in two locations.
 		register_nav_menus(
 			array(
-				'menu-1' => __( 'Primary', '_tw' ),
-				'menu-2' => __( 'Footer Menu', '_tw' ),
+				'menu-1' => __('Primary', '_tw'),
+				'menu-2' => __('Footer Menu', '_tw'),
 			)
 		);
 
@@ -103,62 +104,65 @@ if ( ! function_exists( '_tw_setup' ) ) :
 		);
 
 		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support('customize-selective-refresh-widgets');
 
 		// Add support for editor styles.
-		add_theme_support( 'editor-styles' );
+		add_theme_support('editor-styles');
 
 		// Enqueue editor styles.
-		add_editor_style( 'style-editor.css' );
-		add_editor_style( 'style-editor-extra.css' );
+		add_editor_style('style-editor.css');
+		add_editor_style('style-editor-extra.css');
 
 		// Add support for responsive embedded content.
-		add_theme_support( 'responsive-embeds' );
+		add_theme_support('responsive-embeds');
 
 		// Remove support for block templates.
-		remove_theme_support( 'block-templates' );
+		remove_theme_support('block-templates');
 	}
 endif;
-add_action( 'after_setup_theme', '_tw_setup' );
+add_action('after_setup_theme', '_tw_setup');
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function _tw_widgets_init() {
+function _tw_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => __( 'Footer', '_tw' ),
-			'id'            => 'sidebar-1',
-			'description'   => __( 'Add widgets here to appear in your footer.', '_tw' ),
+			'name' => __('Footer', '_tw'),
+			'id' => 'sidebar-1',
+			'description' => __('Add widgets here to appear in your footer.', '_tw'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
+			'after_widget' => '</section>',
+			'before_title' => '<h2 class="widget-title">',
+			'after_title' => '</h2>',
 		)
 	);
 }
-add_action( 'widgets_init', '_tw_widgets_init' );
+add_action('widgets_init', '_tw_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function _tw_scripts() {
-	wp_enqueue_style( '_tw-style', get_stylesheet_uri(), array(), _TW_VERSION );
-	wp_enqueue_script( '_tw-script', get_template_directory_uri() . '/js/script.min.js', array(), _TW_VERSION, true );
+function _tw_scripts()
+{
+	wp_enqueue_style('_tw-style', get_stylesheet_uri(), array(), _TW_VERSION);
+	wp_enqueue_script('_tw-script', get_template_directory_uri() . '/js/script.min.js', array(), _TW_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', '_tw_scripts' );
+add_action('wp_enqueue_scripts', '_tw_scripts');
 
 /**
  * Enqueue the block editor script.
  */
-function _tw_enqueue_block_editor_script() {
-	if ( is_admin() ) {
+function _tw_enqueue_block_editor_script()
+{
+	if (is_admin()) {
 		wp_enqueue_script(
 			'_tw-editor',
 			get_template_directory_uri() . '/js/block-editor.min.js',
@@ -169,10 +173,10 @@ function _tw_enqueue_block_editor_script() {
 			_TW_VERSION,
 			true
 		);
-		wp_add_inline_script( '_tw-editor', "tailwindTypographyClasses = '" . esc_attr( _TW_TYPOGRAPHY_CLASSES ) . "'.split(' ');", 'before' );
+		wp_add_inline_script('_tw-editor', "tailwindTypographyClasses = '" . esc_attr(_TW_TYPOGRAPHY_CLASSES) . "'.split(' ');", 'before');
 	}
 }
-add_action( 'enqueue_block_assets', '_tw_enqueue_block_editor_script' );
+add_action('enqueue_block_assets', '_tw_enqueue_block_editor_script');
 
 /**
  * Add the Tailwind Typography classes to TinyMCE.
@@ -180,11 +184,13 @@ add_action( 'enqueue_block_assets', '_tw_enqueue_block_editor_script' );
  * @param array $settings TinyMCE settings.
  * @return array
  */
-function _tw_tinymce_add_class( $settings ) {
+function _tw_tinymce_add_class($settings)
+{
 	$settings['body_class'] = _TW_TYPOGRAPHY_CLASSES;
 	return $settings;
 }
-add_filter( 'tiny_mce_before_init', '_tw_tinymce_add_class' );
+add_filter('tiny_mce_before_init', '_tw_tinymce_add_class');
+
 
 /**
  * Custom template tags for this theme.
@@ -195,3 +201,13 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
+
+function register_my_menus()
+{
+	register_nav_menus(
+		array(
+			'header-menu' => __('Header Menu'),
+		)
+	);
+}
+add_action('init', 'register_my_menus');
