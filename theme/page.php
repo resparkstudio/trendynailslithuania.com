8b536ac77d89bbe3392e078d432b3c81c09d500b
Rokas Reizgys <rokas.student@gmail.com>
2024-10-16 11:20:29 +0300

    make footer stick to bottom

--- theme/page.php
+++ theme/page.php
@@ -14,27 +14,27 @@
 get_header();
 ?>
 
-	<section id="primary">
-		<main id="main">
+<section id="primary">
+	<main id="main">
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+		<?php
+		/* Start the Loop */
+		while (have_posts()):
+			the_post();
 
-				get_template_part( 'template-parts/content/content', 'page' );
+			get_template_part('template-parts/content/content', 'page');
 
-				// If comments are open, or we have at least one comment, load
-				// the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
+			// If comments are open, or we have at least one comment, load
+			// the comment template.
+			if (comments_open() || get_comments_number()) {
+				comments_template();
+			}
 
-			endwhile; // End of the loop.
-			?>
+		endwhile; // End of the loop.
+		?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+	</main><!-- #main -->
+</section><!-- #primary -->
 
 <?php
 get_footer();
