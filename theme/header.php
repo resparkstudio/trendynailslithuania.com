b7685220936f0d8cd785e539f9a907ffb2b634c6
Rokas Reizgys <rokas.student@gmail.com>
2024-11-11 16:42:01 +0200

    style count icons

--- theme/header.php
+++ theme/header.php
@@ -110,7 +110,7 @@ Template Name: Header
 									stroke="#201F1F" stroke-width="1.1" stroke-linecap="round" />
 							</svg>
 							<span id="wishlist-count"
-								class="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-black text-white rounded-full text-xs font-bold w-5 h-5 flex items-center justify-center">
+								class="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-black text-white rounded-full text-[0.5rem] font-bold w-3 h-3 flex items-center justify-center">
 								<?php echo count(custom_get_wishlist()); // PHP function to get wishlist count ?>
 							</span>
 						</a>
@@ -145,7 +145,7 @@ Template Name: Header
 									fill="#201F1F" mask="url(#path-1-inside-1_192_1899)" />
 							</svg>
 							<span id="cart-count"
-								class="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-black text-white rounded-full text-xs font-bold w-5 h-5 flex items-center justify-center">
+								class="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-black text-white rounded-full text-[0.5rem] font-bold w-3 h-3 flex items-center justify-center">
 								<?php echo WC()->cart->get_cart_contents_count(); ?>
 							</span>
 						</a>
