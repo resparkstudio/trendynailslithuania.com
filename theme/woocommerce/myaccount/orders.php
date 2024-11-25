386cff31fdfbcdc0de9a3c6942d8ae7f770aac4e
Rokas Reizgys <rokas.student@gmail.com>
2024-11-25 18:07:59 +0200

    fix tempalte headers

--- theme/woocommerce/myaccount/orders.php
+++ theme/woocommerce/myaccount/orders.php
@@ -1,4 +1,22 @@
-<?php if ($has_orders): ?>
+<?php
+/**
+ * Orders
+ *
+ * Shows orders on the account page.
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/orders.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 9.2.0
+ */
+if ($has_orders): ?>
 
 	<div class="overflow-x-auto">
 		<table class="min-w-full table-auto border-collapse border border-deep-dark-gray">
