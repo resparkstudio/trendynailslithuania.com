f3a0df34db8a266f7ad9f411fdf84fbeabbfec94
Rokas Reizgys <rokas.student@gmail.com>
2024-11-26 14:29:45 +0200

    add svg support

--- theme/functions.php
+++ theme/functions.php
@@ -1456,3 +1456,49 @@ function update_shipping_method_on_refresh($posted_data)
 		WC()->cart->calculate_totals();
 	}
 }
+
+
+// Allow SVG Uploads in WordPress
+function allow_svg_upload($mimes)
+{
+	// Add SVG to allowed mime types
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+}
+add_filter('upload_mimes', 'allow_svg_upload');
+
+// Allow SVG
+add_filter('wp_check_filetype_and_ext', function ($data, $file, $filename, $mimes) {
+
+	global $wp_version;
+	if ($wp_version !== '4.7.1') {
+		return $data;
+	}
+
+	$filetype = wp_check_filetype($filename, $mimes);
+
+	return [
+		'ext' => $filetype['ext'],
+		'type' => $filetype['type'],
+		'proper_filename' => $data['proper_filename']
+	];
+
+}, 10, 4);
+
+function cc_mime_types($mimes)
+{
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+function fix_svg()
+{
+	echo '<style type="text/css">
+		  .attachment-266x266, .thumbnail img {
+			   width: 100% !important;
+			   height: auto !important;
+		  }
+		  </style>';
+}
+add_action('admin_head', 'fix_svg');
